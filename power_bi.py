7953c3a959d7a5e307d9dd23fec9c60092f53422
GOODRICH OKORO <okorogoodrich@gmail.com>
2023-09-01 12:31:17 +0100

    added db3

--- power_bi.py
+++ power_bi.py
@@ -16,5 +16,11 @@ with st.expander(':red[E-Commerce Industry]', expanded=True):
               frameborder="0" allowFullScreen="true"></iframe>"""
   components.html(power, height=1010, width=1800)
   
+with st.expander(':red[Investment Industry]', expanded=True):
+  power = """<iframe title="Report Section" width="1200" height="1000" 
+              src="https://app.powerbi.com/view?r=eyJrIjoiMTU0MTI4YmUtMjQzYi00OGNjLTk5MzgtMmNhM2UwMzlkNDI3IiwidCI6ImNlMzBlNGMzLWM4NjItNGVlZC1hMzdjLWU3NmJjODNhY2ZmYSJ9" 
+              frameborder="0" allowFullScreen="true"></iframe>"""
+  components.html(power, height=1010, width=1800)
+  
 
-#   <iframe title="Report Section" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMGFmNjA5ZTUtNTFjMS00ODVkLTgyMDctMWNmMzBjZTdhM2ZmIiwidCI6ImNlMzBlNGMzLWM4NjItNGVlZC1hMzdjLWU3NmJjODNhY2ZmYSJ9" frameborder="0" allowFullScreen="true"></iframe>
+# <iframe title="clife_investments" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMTU0MTI4YmUtMjQzYi00OGNjLTk5MzgtMmNhM2UwMzlkNDI3IiwidCI6ImNlMzBlNGMzLWM4NjItNGVlZC1hMzdjLWU3NmJjODNhY2ZmYSJ9" frameborder="0" allowFullScreen="true"></iframe>
