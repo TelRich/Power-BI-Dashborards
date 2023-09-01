9db07ec3e717609d68cce9e68d3c34ebfabf98be
GOODRICH OKORO <okorogoodrich@gmail.com>
2023-09-01 12:06:21 +0100

    added first dashboard

--- power_bi.py
+++ power_bi.py
@@ -1,1 +1,10 @@
 import streamlit as st
+import streamlit.components.v1 as components
+
+with st.expander(':red[Supply Chain Industry]', expanded=True):
+  power = """<iframe title="Report Section" width="1400" height="1200" 
+              src="https://app.powerbi.com/view?r=eyJrIjoiZWEwNDMzMzItMTdmYS00Yzg5LTliOTYtMjhjMTg2MTQxZjcwIiwidCI6ImNlMzBlNGMzLWM4NjItNGVlZC1hMzdjLWU3NmJjODNhY2ZmYSJ9" 
+              frameborder="0" allowFullScreen="true"></iframe>"""
+  components.html(power, height=800, width=1800)
+  
+#   <iframe title="supply_chain" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZWEwNDMzMzItMTdmYS00Yzg5LTliOTYtMjhjMTg2MTQxZjcwIiwidCI6ImNlMzBlNGMzLWM4NjItNGVlZC1hMzdjLWU3NmJjODNhY2ZmYSJ9" frameborder="0" allowFullScreen="true"></iframe>
