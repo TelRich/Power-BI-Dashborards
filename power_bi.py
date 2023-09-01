80cddccba5c0754e4015fe473cbd50a915b77099
GOODRICH OKORO <okorogoodrich@gmail.com>
2023-09-01 12:11:16 +0100

    adjust page size

--- power_bi.py
+++ power_bi.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
+# Setting th epage size and title
+st.set_page_config(layout='wide', page_title='Power BI Portfolio')
+
 with st.expander(':red[Supply Chain Industry]', expanded=True):
   power = """<iframe title="Report Section" width="1400" height="1200" 
               src="https://app.powerbi.com/view?r=eyJrIjoiZWEwNDMzMzItMTdmYS00Yzg5LTliOTYtMjhjMTg2MTQxZjcwIiwidCI6ImNlMzBlNGMzLWM4NjItNGVlZC1hMzdjLWU3NmJjODNhY2ZmYSJ9" 
