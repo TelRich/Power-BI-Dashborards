cc2338fcc969e1609abc023286a687e6d4f41023
GOODRICH OKORO <okorogoodrich@gmail.com>
2023-09-01 13:01:13 +0100

    added other visuals

--- power_bi.py
+++ power_bi.py
@@ -21,6 +21,35 @@ with st.expander(':red[Investment Industry]', expanded=True):
               src="https://app.powerbi.com/view?r=eyJrIjoiMTU0MTI4YmUtMjQzYi00OGNjLTk5MzgtMmNhM2UwMzlkNDI3IiwidCI6ImNlMzBlNGMzLWM4NjItNGVlZC1hMzdjLWU3NmJjODNhY2ZmYSJ9" 
               frameborder="0" allowFullScreen="true"></iframe>"""
   components.html(power, height=1010, width=1800)
-  
 
-# <iframe title="clife_investments" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiMTU0MTI4YmUtMjQzYi00OGNjLTk5MzgtMmNhM2UwMzlkNDI3IiwidCI6ImNlMzBlNGMzLWM4NjItNGVlZC1hMzdjLWU3NmJjODNhY2ZmYSJ9" frameborder="0" allowFullScreen="true"></iframe>
+with st.expander(':red[Education Industry]', expanded=True):
+  power = """<iframe title="Report Section" width="1200" height="1000" 
+              src="https://app.powerbi.com/view?r=eyJrIjoiYTFlZTIwZGQtYzA4NS00YTNiLWJkZGEtZTQ3MmNjMWIyNmI1IiwidCI6ImNlMzBlNGMzLWM4NjItNGVlZC1hMzdjLWU3NmJjODNhY2ZmYSJ9" 
+              frameborder="0" allowFullScreen="true"></iframe>"""
+  components.html(power, height=1010, width=1800)
+
+with st.expander(':red[Education Industry]', expanded=True):
+  power = """<iframe title="Report Section" width="1200" height="1000" 
+              src="https://app.powerbi.com/view?r=eyJrIjoiYTIyZWYyY2YtMWFmZS00MTllLWJmNzItMWMwYTRkZmRmOTIwIiwidCI6ImNlMzBlNGMzLWM4NjItNGVlZC1hMzdjLWU3NmJjODNhY2ZmYSJ9" 
+              frameborder="0" allowFullScreen="true"></iframe>"""
+  components.html(power, height=1010, width=1800)
+
+with st.expander(':red[Sales Industry]', expanded=True):
+  power = """<iframe title="Report Section" width="1200" height="1000" 
+              src="https://app.powerbi.com/view?r=eyJrIjoiZjcyYTY2ZGUtYWJmYS00YzNkLWI5ZGItYmM5NjhjNjY4MmE3IiwidCI6ImNlMzBlNGMzLWM4NjItNGVlZC1hMzdjLWU3NmJjODNhY2ZmYSJ9" 
+              frameborder="0" allowFullScreen="true"></iframe>"""
+  components.html(power, height=1010, width=1800)   
+
+with st.expander(':red[Sales Industry]', expanded=True):
+  power = """<iframe title="Report Section" width="1200" height="1000" 
+              src="https://app.powerbi.com/view?r=eyJrIjoiNTg3MTM1MmMtMWI1OS00OTY1LWJmODctOTJmMzM3MDk3NDgyIiwidCI6ImNlMzBlNGMzLWM4NjItNGVlZC1hMzdjLWU3NmJjODNhY2ZmYSJ9" 
+              frameborder="0" allowFullScreen="true"></iframe>"""
+  components.html(power, height=1010, width=1800)
+
+with st.expander(':red[Sales Industry]', expanded=True):
+  power = """<iframe title="Report Section" width="1200" height="1000" 
+              src="https://app.powerbi.com/view?r=eyJrIjoiODAyYjM1NTgtMWQwOC00MTA1LTk5NGMtYTk3YWFiN2EzZDU3IiwidCI6ImNlMzBlNGMzLWM4NjItNGVlZC1hMzdjLWU3NmJjODNhY2ZmYSJ9" 
+              frameborder="0" allowFullScreen="true"></iframe>"""
+  components.html(power, height=1010, width=1800)   
+
+# <iframe title="Report Section" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZjcyYTY2ZGUtYWJmYS00YzNkLWI5ZGItYmM5NjhjNjY4MmE3IiwidCI6ImNlMzBlNGMzLWM4NjItNGVlZC1hMzdjLWU3NmJjODNhY2ZmYSJ9" frameborder="0" allowFullScreen="true"></iframe>
