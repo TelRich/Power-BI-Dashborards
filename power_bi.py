f7b746f351b5407a6c9d18b3bb2805c041ce3af6
GOODRICH OKORO <okorogoodrich@gmail.com>
2023-09-01 12:14:36 +0100

    adjust size of dashboard1

--- power_bi.py
+++ power_bi.py
@@ -5,9 +5,9 @@ import streamlit.components.v1 as components
 st.set_page_config(layout='wide', page_title='Power BI Portfolio')
 
 with st.expander(':red[Supply Chain Industry]', expanded=True):
-  power = """<iframe title="Report Section" width="1400" height="1200" 
+  power = """<iframe title="Report Section" width="1200" height="1000" 
               src="https://app.powerbi.com/view?r=eyJrIjoiZWEwNDMzMzItMTdmYS00Yzg5LTliOTYtMjhjMTg2MTQxZjcwIiwidCI6ImNlMzBlNGMzLWM4NjItNGVlZC1hMzdjLWU3NmJjODNhY2ZmYSJ9" 
               frameborder="0" allowFullScreen="true"></iframe>"""
-  components.html(power, height=800, width=1800)
+  components.html(power, height=835, width=1800)
   
 #   <iframe title="supply_chain" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZWEwNDMzMzItMTdmYS00Yzg5LTliOTYtMjhjMTg2MTQxZjcwIiwidCI6ImNlMzBlNGMzLWM4NjItNGVlZC1hMzdjLWU3NmJjODNhY2ZmYSJ9" frameborder="0" allowFullScreen="true"></iframe>
