3c2ab81b564d3dbb7e2a73b0b614aa1288c4afb1
GOODRICH OKORO <okorogoodrich@gmail.com>
2023-09-01 12:21:16 +0100

    final adjustment for db1

--- power_bi.py
+++ power_bi.py
@@ -8,6 +8,13 @@ with st.expander(':red[Supply Chain Industry]', expanded=True):
   power = """<iframe title="Report Section" width="1200" height="1000" 
               src="https://app.powerbi.com/view?r=eyJrIjoiZWEwNDMzMzItMTdmYS00Yzg5LTliOTYtMjhjMTg2MTQxZjcwIiwidCI6ImNlMzBlNGMzLWM4NjItNGVlZC1hMzdjLWU3NmJjODNhY2ZmYSJ9" 
               frameborder="0" allowFullScreen="true"></iframe>"""
-  components.html(power, height=835, width=1800)
+  components.html(power, height=1010, width=1800)
   
-#   <iframe title="supply_chain" width="600" height="373.5" src="https://app.powerbi.com/view?r=eyJrIjoiZWEwNDMzMzItMTdmYS00Yzg5LTliOTYtMjhjMTg2MTQxZjcwIiwidCI6ImNlMzBlNGMzLWM4NjItNGVlZC1hMzdjLWU3NmJjODNhY2ZmYSJ9" frameborder="0" allowFullScreen="true"></iframe>
+# with st.expander(':red[Supply Chain Industry]', expanded=True):
+#   power = """<iframe title="Report Section" width="1200" height="1000" 
+#               src="https://app.powerbi.com/view?r=eyJrIjoiZWEwNDMzMzItMTdmYS00Yzg5LTliOTYtMjhjMTg2MTQxZjcwIiwidCI6ImNlMzBlNGMzLWM4NjItNGVlZC1hMzdjLWU3NmJjODNhY2ZmYSJ9" 
+#               frameborder="0" allowFullScreen="true"></iframe>"""
+#   components.html(power, height=835, width=1800)
+  
+
+  
